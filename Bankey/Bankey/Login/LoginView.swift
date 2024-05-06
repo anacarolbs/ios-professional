43bd1bc715aa97e5f16ae3d488f076712768bda8
Ana Carolina Barbosa de Souza <anacarolinabarbosadesouza@MBP-de-Ana.rede.jrs>
2024-05-06 15:22:20 -0300

    My second commit

---
+++ Bankey/Bankey/Login/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  Bankey
+//
+//  Created by Ana Carolina Barbosa de Souza on 06/05/24.
+//
+
+import Foundation
---
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Ana Carolina Barbosa de Souza on 06/05/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
