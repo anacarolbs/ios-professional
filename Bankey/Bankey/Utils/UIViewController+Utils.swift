9138c6d3d7259b3e4212b3eb135150a0a7cfe9a5
Ana Carolina Barbosa de Souza <anacarolinabarbosadesouza@MBP-de-Ana.rede.jrs>
2024-05-09 14:57:01 -0300

    feat: add tab bar

--- Bankey/Bankey/Utils/UIViewController+Utils.swift
+++ Bankey/Bankey/Utils/UIViewController+Utils.swift
@@ -7,19 +7,29 @@
 
 import UIKit
 
+//extension UIViewController {
+//    func setStatusBar() {
+//        let statusBarSize = UIApplication.shared.statusBarFrame.size // deprecated but OK
+//        let frame = CGRect(origin: .zero, size: statusBarSize)
+//        let statusbarView = UIView(frame: frame)
+//
+//        statusbarView.backgroundColor = appColor
+//        view.addSubview(statusbarView)
+//    }
+    
 extension UIViewController {
     func setStatusBar() {
-        let statusBarSize = UIApplication.shared.statusBarFrame.size // deprecated but OK
-        let frame = CGRect(origin: .zero, size: statusBarSize)
-        let statusbarView = UIView(frame: frame)
-
-        statusbarView.backgroundColor = appColor
-        view.addSubview(statusbarView)
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground() // to hide Navigation Bar Line also
+        navBarAppearance.backgroundColor = appColor
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
     }
-    
+}
+
     func setTabBarImage(imageName: String, title: String) {
         let configuration = UIImage.SymbolConfiguration(scale: .large)
         let image = UIImage(systemName: imageName, withConfiguration: configuration)
         tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
     }
-}
+
