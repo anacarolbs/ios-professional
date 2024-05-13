8d765231fe8490b62e6ac6eefae46b2dabeb0df9
Ana Carolina Barbosa de Souza <anacarolinabarbosadesouza@MacBook-Pro-de-Ana.local>
2024-05-13 10:58:58 -0300

    feat: handle network errors

--- Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
+++ Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
@@ -207,7 +207,11 @@ extension AccountSummaryViewController {
 //                self.configureTableHeaderView(with: profile)
 //                self.tableView.reloadData()
             case .failure(let error):
-                print(error.localizedDescription)
+                self.displayError(error)
+//                self.showErrorAlert()
+                //switch
+                
+//                print(error.localizedDescription)
             }
             group.leave()
         }
@@ -220,7 +224,8 @@ extension AccountSummaryViewController {
 //                self.configureTableCells(with: accounts)
 //                self.tableView.reloadData()
             case .failure(let error):
-                print(error.localizedDescription)
+                self.displayError(error)
+//                print(error.localizedDescription)
             }
             group.leave()
         }
@@ -256,6 +261,30 @@ extension AccountSummaryViewController {
                                          accountName: $0.name,
                                          balance: $0.amount)
         }
+        
+        private func displayError(_ error: NetworkError) {
+            let title: String
+            let message: String
+            switch error {
+            case .serverError:
+                title = "Server Error"
+                message = "Ensure you are connected to the internet. Please try again."
+            case .decodingError:
+                title: "Decoding error"
+                messageL "We could not process your request. Please try again."
+            }
+            self.showErrorAlert(title: title, message: message)
+        }
+    }
+    
+    private func showErrorAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
     }
 }
 
