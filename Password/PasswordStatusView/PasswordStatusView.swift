2df4c2225a8114a672d6753bb85567897edb018d
Ana Carolina Barbosa de Souza <souzaanacarolb@gmail.com>
2024-05-15 12:41:14 -0300

    feat: add loss of focus interactions

--- Password/PasswordStatusView/PasswordStatusView.swift
+++ Password/PasswordStatusView/PasswordStatusView.swift
@@ -20,7 +20,7 @@ class PasswordStatusView: UIView {
     let specialCharacterCriteriaView = PasswordCriteriaView(text: "special character (e.g. !@#$%^)")
     
     // Used to determine if we reset criteria back to empty state (⚪️).
-    private var shouldResetCriteria: Bool = true
+    var shouldResetCriteria: Bool = true
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -125,21 +125,55 @@ extension PasswordStatusView {
             : lengthCriteriaView.reset()
             
             uppercaseMet
-                ? uppercaseCriteriaView.isCriteriaMet = true
-                : uppercaseCriteriaView.reset()
+            ? uppercaseCriteriaView.isCriteriaMet = true
+            : uppercaseCriteriaView.reset()
             
             lowercaseMet
-                ? lowerCaseCriteriaView.isCriteriaMet = true
-                : lowerCaseCriteriaView.reset()
-
+            ? lowerCaseCriteriaView.isCriteriaMet = true
+            : lowerCaseCriteriaView.reset()
+            
             digitMet
-                ? digitCriteriaView.isCriteriaMet = true
-                : digitCriteriaView.reset()
+            ? digitCriteriaView.isCriteriaMet = true
+            : digitCriteriaView.reset()
             
             specialCharacterMet
-                ? specialCharacterCriteriaView.isCriteriaMet = true
-                : specialCharacterCriteriaView.reset()
+            ? specialCharacterCriteriaView.isCriteriaMet = true
+            : specialCharacterCriteriaView.reset()
+        } else {
+            // Focus lost (✅ or ❌)
+            lengthCriteriaView.isCriteriaMet = lengthAndNoSpaceMet
+            uppercaseCriteriaView.isCriteriaMet = uppercaseMet
+            lowerCaseCriteriaView.isCriteriaMet = lowercaseMet
+            digitCriteriaView.isCriteriaMet = digitMet
+            specialCharacterCriteriaView.isCriteriaMet = specialCharacterMet
         }
+    }
+}
+    
+    func validate(_ text: String) -> Bool {
+        let uppercaseMet = PasswordCriteria.uppercaseMet(text)
+        let lowercaseMet = PasswordCriteria.lowercaseMet(text)
+        let digitMet = PasswordCriteria.digitMet(text)
+        let specialCharacterMet = PasswordCriteria.specialCharacterMet(text)
+
+        let checkable = [uppercaseMet, lowercaseMet, digitMet, specialCharacterMet]
+        let metCriteria = checkable.filter { $0 }
+        let lengthAndNoSpaceMet = PasswordCriteria.lengthAndNoSpaceMet(text)
+        
+        if lengthAndNoSpaceMet && metCriteria.count >= 3 {
+            return true
+        }
+
+        return false
+    }
+        
+//        func reset() {
+//            lengthCriteriaView.reset()
+//            uppercaseCriteriaView.reset()
+//            lowerCaseCriteriaView.reset()
+//            digitCriteriaView.reset()
+//            specialCharacterCriteriaView.reset()
+//        }
 //        if shouldResetCriteria {
 //            // Inline validation (✅ or ⚪️)
 //            if  lengthAndNoSpaceMet {
@@ -148,5 +182,5 @@ extension PasswordStatusView {
 //                lengthCriteriaView.reset()
 //            }
 //        }
-    }
-}
+
+
